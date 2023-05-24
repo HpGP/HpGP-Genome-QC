454ab6b11befb5932c71d150a6ed17d9fdcc270b
Difei Wang <49001003+wangdi2016@users.noreply.github.com>
2023-05-23 23:36:10 -0400

    Add script for busco

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HpGP-Genome-QC
+
+## 1. Install busco package from https://busco.ezlab.org/
+
+## 2. Download busco database https://busco-data.ezlab.org/v5/data/lineages/campylobacterales_odb10.2020-03-06.tar.gz
+
+## 3. Run run.busco.sh
---
+++ run.busco.sh
@@ -0,0 +1,41 @@
+# conda activate bacgen3
+
+#source /Users/wangdi/apps/anaconda3/bin/activate bacgen3
+source /data/wangdi/bin/anaconda3/bin/activate bacgen3
+
+LIST=$1
+
+echo "## Complete BUSCOs (C)" > ${LIST}.busco.sum.txt
+echo "## Complete and single-copy BUSCOs (S)" >> ${LIST}.busco.sum.txt
+echo "## Complete and duplicated BUSCOs (D)" >> ${LIST}.busco.sum.txt
+echo "## Fragmented BUSCOs (F)" >> ${LIST}.busco.sum.txt
+echo "## Missing BUSCOs (M)" >> ${LIST}.busco.sum.txt
+echo "## Total BUSCO groups searched" >> ${LIST}.busco.sum.txt
+echo "## strain,C,S,D,F,M,n" >> ${LIST}.busco.sum.txt
+
+## loop list
+for f in `cat $LIST`
+do
+
+## copy fasta
+cat ./fna/${f}.fasta > ${f}.fna
+
+echo "== $f =="
+
+##
+## download busco db
+##
+## https://busco-data.ezlab.org/v5/data/lineages/campylobacterales_odb10.2020-03-06.tar.gz
+busco -m genome -i ${f}.fna -o ${f} -l campylobacterales_odb10
+
+## length
+len=$(seqlen.sh ${f}.fna | awk '{print $2}')
+
+## combine all
+line=$(grep "C:" ${f}/short_summary.specific.campylobacterales_odb10.${f}.txt | sed 's/\[/,/; s/\]//' | sed 's/C://; s/S://; s/D://; s/F://; s/M://; s/n://'| awk '{print $1}')
+echo "$f,$len,$line" >> ${LIST}.busco.sum.txt
+
+rm -rf ${f}.fna
+
+done
+
